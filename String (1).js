a9264ae0878eb74b2c7d1728b7efc8c38032f073
Aiman khatoon <113781674+aimankhatoon1@users.noreply.github.com>
2023-04-18 15:35:25 +0500

    Add files via upload

---
+++ String (1).js
@@ -0,0 +1,51 @@
+/*Length() function*/
+let str="Aiman Khatoon";
+console.log(str.length);
+/*Slice() function*/
+let sli="Football,Cricket,Hockey"
+console.log(sli.slice(9,16));
+/*substring() function*/
+let subst="Aiman,Mahnoor,Munnaza";
+console.log(subst.substring(-1,5));
+/*substr() function*/
+let subst1="Aiman,Mahnoor,Munnaza";
+console.log(subst.substr(0,5));
+/*replace() function*/
+let name="My name is Aiman.";
+console.log(name.replace("Aiman","Mahnoor"));
+/*touppercase() function*/
+let text1="My name is Aiman.";
+console.log(text1.toUpperCase()); 
+/*toLowerCase() function*/
+let text2="My name is Aiman.";
+console.log(text2.toLowerCase());
+/*concat() function*/
+let text3 ="Iqra";
+let text4="Basharat";
+console.log(text3.concat(text4));
+/*Trim() function*/
+let text="    Comsats       ";
+console.log(text.trim());
+/*Trimstart() function*/
+let text5="    Zee       ";
+console.log(text5.trimStart());
+/*Trim() function*/
+let text6="    Hani       ";
+console.log(text6.trimEnd());
+/*Padstart() function*/
+let text7 = "5";
+console.log(text7.padStart(7,"Farhan"));
+/*Padend() function*/
+let text8 = "5";
+console.log(text8.padEnd(7,"Farhan"));
+/*CharAt() function*/
+let string="Hammad";
+console.log(string.charAt(5));
+/*CharcodeAt() function*/
+let string1="Hammad";
+console.log(string.charCodeAt(5));
+
+
+
+
+
